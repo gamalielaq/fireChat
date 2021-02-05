03fe7c119a226dc4d3ba80bf29eeb3dc5963ddec
Gamaliel <gabanto@sigcomt.com>
2021-02-05 18:55:37 -0500

    Cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCVTDpcazA6Dfc22nxSg3oi2EO2AfznJz0",
+    authDomain: "firechat-4c8c1.firebaseapp.com",
+    projectId: "firechat-4c8c1",
+    storageBucket: "firechat-4c8c1.appspot.com",
+    messagingSenderId: "583734051673",
+    appId: "1:583734051673:web:e213d2f66c4e683a190248"
+  }
 };
